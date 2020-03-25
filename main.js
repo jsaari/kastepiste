22634747aaed77afd1a87ddd5e81a136ec445a4e
Jukka Saari <jukka.saari@iki.fi>
2020-03-25 21:24:18 +0200

    remove utils.js

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ var timeFormat = 'MM/DD/YYYY HH:mm';
 
 var WEEK = 1000*60*60*24*7;
 var color = Chart.helpers.color;
+window.chartColors = {
+	red: 'rgb(255, 99, 132)',
+	orange: 'rgb(255, 159, 64)',
+	yellow: 'rgb(255, 205, 86)',
+	green: 'rgb(75, 192, 192)',
+	blue: 'rgb(54, 162, 235)',
+	purple: 'rgb(153, 102, 255)',
+	grey: 'rgb(201, 203, 207)'
+};
 var chartConfig = {
     type: 'line',
     data: {
